70bd82a552dbfaf44bd8778bc46639067cfd882f
Mantarus <m.mantarus@gmail.com>
2018-05-28 22:07:37 +0400

    Refactor and document Game class

--- game.rb
+++ game.rb
@@ -1,47 +1,62 @@
+# class Game
 class Game
-  def initialize (player_1, player_2)
-    @players= [[player_1, BattleField.new, 0],
-               [player_2, BattleField.new, 0]]
-    @players.each {|p| reset p}
+  def initialize(player1, player2)
+    @players = [
+      [player1, BattleField.new, 0],
+      [player2, BattleField.new, 0]
+    ]
+    @players.each { |p| reset p }
     @players.shuffle!
     @game_over = false
   end
 
-  def reset p
-    print(p[0].name, " game setup\n")
-    p[0].reset
-    player_ships = p[1].fleet
-    if !p[1].place_fleet(p[0].place_strategy player_ships)
-      raise "Illegal ship placement"
+  def reset(player_arr)
+    print(player_arr[0].name, " game setup\n")
+    player_arr[0].reset
+    player_ships = player_arr[1].fleet
+    if !player_arr[1].place_fleet(player_arr[0].place_strategy(player_ships))
+      raise 'Illegal ship placement'
     else
-      puts "Ships placed"
+      puts 'Ships placed'
     end
   end
 
-  # Задание 33 start
   def start
-    lastshots = []
-    while ! @game_over
+    last_shots = []
+    until @game_over
+
+      # Init players
       p1 = @players[0]
       p2 = @players[1]
+
+      # Increase turn counter
       p1[2] += 1
-      print("Step #{p1[2]} of player ",p1[0].name, "\n")
+      print("Step #{p1[2]} of player ", p1[0].name, "\n")
+
+      # Cure all current player's ships
       p1[1].cure
-      p1[1].move (p1[0].ship_move_strategy p1[1].remains)
+
+      # Player moves or rotates one of his ships and then shoots
+      p1[1].move(p1[0].ship_move_strategy(p1[1].remains))
       # p2[1].print_field
       shot = p1[0].shot_strategy
-      if lastshots.include? shot
-        puts "Illegal shot"
-        res = "miss"
+
+      # Check for illegal shot and, if legal, make a shot
+      if last_shots.include? shot
+        puts 'Illegal shot'
+        res = 'miss'
       else
-        lastshots.push shot
+        last_shots.push shot
         res = p2[1].shoot shot
       end
-      print(shot, " ", res, "\n")
-      if res == "miss"
+
+      print(shot, ' ', res, "\n")
+
+      # Check hit. If miss, pass to the opponent, else check game over condition
+      if res == 'miss'
         p1[0].miss
         @players.reverse!
-        lastshots = []
+        last_shots = []
       else
         p1[0].hit res
         @game_over = p2[1].game_over?
@@ -53,6 +68,3 @@ class Game
     end
   end
 end
-# конец описания класса Game
-################################################################################
-
