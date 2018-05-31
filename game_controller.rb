d4601b0df59f3b0db12e41588fd1a1fd1b24151f
Mantarus <m.mantarus@gmail.com>
2018-05-31 23:48:13 +0400

    Handle errors

--- game_controller.rb
+++ game_controller.rb
@@ -13,10 +13,11 @@ def add_module_definition(class_definition, module_name)
   "module #{module_name}\n#{class_definition}\nend"
 end
 
-player_str = File.read('player.txt')
+default_player_str = File.read('test_strategies/default.txt')
+custom_player_str = File.read('test_strategies/cannot_compile.txt')
 
-class_a_str = player_str
-class_b_str = player_str
+class_a_str = default_player_str
+class_b_str = custom_player_str
 
 file_a = generate_file_name
 file_b = generate_file_name
@@ -27,16 +28,27 @@ module_b = add_module_definition(class_b_str, 'ModuleB')
 open(file_a, 'w') { |f| f.puts module_a }
 open(file_b, 'w') { |f| f.puts module_b }
 
+begin
 load file_a
 load file_b
+rescue SyntaxError => error
+  puts error.message
+  FileUtils.remove file_a
+  FileUtils.remove file_b
+  return
+end
 
 FileUtils.remove file_a
 FileUtils.remove file_b
 
 srand
 
-p1 = ModuleA::Player.new('Ivan', true)
-p2 = ModuleB::Player.new('Feodor')
-g = Game.new(p1, p2)
-g.start
-puts g.read_game_log
+begin
+  p1 = ModuleA::Player.new('Player 1')
+  p2 = ModuleB::Player.new('Player 2')
+  g = Game.new(p1, p2)
+  g.start
+  # puts g.read_game_log
+rescue => error
+  puts error.message
+end
