4aa74068e0aeedc9cf0f8a6071075d64f65928c8
Mantarus <m.mantarus@gmail.com>
2018-06-04 19:58:14 +0400

    Modify GameController and Sinatra controller

--- game_controller.rb
+++ game_controller.rb
@@ -6,6 +6,7 @@ require './ship.rb'
 require './response.rb'
 
 require 'FileUtils'
+require 'timeout'
 
 class GameController
 
@@ -14,6 +15,30 @@ class GameController
     @strategy1 = strategy1
     @strategy2 = strategy2
   end
+
+  def play_game
+    module1 = generate_module_name
+    module2 = generate_module_name
+    file1 = generate_file_name
+    file2 = generate_file_name
+
+    begin
+      result = Timeout::timeout(2) do
+        p1 = generate_player_object('P1', @strategy1, module1, file1)
+        p2 = generate_player_object('P2', @strategy2, module2, file2)
+
+        g = Game.new(p1, p2)
+        result = g.start
+        clean_resources(module1, module2, file1, file2)
+        Response.new(true, result.winner, result.game_log, nil)
+      end
+    rescue Exception => e
+      clean_resources(module1, module2, file1, file2)
+      Response.new(false, nil, nil, handle_errors(e))
+    end
+  end
+
+  private
 
   def generate_player_object(player_name, strategy, module_name, file_name)
     strategy = add_module_definition(strategy, module_name)
@@ -31,82 +56,10 @@ class GameController
       Object.send(:remove_const, module2)
     rescue NameError
     end
+
     FileUtils.remove file1
     FileUtils.remove file2
   end
-
-  def play_game
-    module1 = generate_module_name
-    module2 = generate_module_name
-    file1 = generate_file_name
-    file2 = generate_file_name
-
-    begin
-      p1 = generate_player_object('P1', @strategy1, module1, file1)
-      p2 = generate_player_object('P2', @strategy2, module2, file2)
-
-      g = Game.new(p1, p2)
-      result = g.start
-      clean_resources(module1, module2, file1, file2)
-      Response.new(true, result.winner, result.game_log, nil)
-    rescue Exception => e
-      clean_resources(module1, module2, file1, file2)
-      Response.new(false, nil, nil, e.message)
-    end
-  end
-
-  # def play_game
-  #
-  #   module1 = generate_module_name
-  #   module2 = generate_module_name
-  #
-  #   @strategy1 = add_module_definition(@strategy1, module1)
-  #   @strategy2 = add_module_definition(@strategy2, module2)
-  #
-  #   file1 = generate_file_name
-  #   file2 = generate_file_name
-  #
-  #   open(file1, 'w') { |f| f.puts @strategy1 }
-  #   open(file2, 'w') { |f| f.puts @strategy2 }
-  #
-  #   begin
-  #   load file1
-  #   rescue SyntaxError => error
-  #     message = handle_syntax_error(error, file1)
-  #     FileUtils.remove file2
-  #     return Response.new(false, nil, nil, message)
-  #   end
-  #
-  #   begin
-  #     load file2
-  #   rescue SyntaxError => error
-  #     message = handle_syntax_error(error, file2)
-  #     FileUtils.remove file1
-  #     return Response.new(false, nil, nil, message)
-  #   end
-  #
-  #   FileUtils.remove file1
-  #   FileUtils.remove file2
-  #
-  #   begin
-  #     p1 = eval "#{module1}::Player.new('P1')"
-  #     p2 = eval "#{module2}::Player.new('P2')"
-  #     g = Game.new(p1, p2)
-  #     result = g.start
-  #
-  #     Object.send(:remove_const, module1)
-  #     Object.send(:remove_const, module2)
-  #
-  #     Response.new(true, result.winner, result.game_log, nil)
-  #   rescue => error
-  #     Object.send(:remove_const, module1)
-  #     Object.send(:remove_const, module2)
-  #     return Response.new(false, nil, nil, error.message)
-  #   end
-  #
-  # end
-
-  private
 
   def generate_file_name
     "strategy_#{srand}.rb"
@@ -120,9 +73,14 @@ class GameController
     "module #{module_name}\n#{class_definition}\nend"
   end
 
-  def handle_syntax_error(syntax_error, file)
-    FileUtils.remove file
-    syntax_error.message
+  def handle_errors(error)
+    if error.is_a?(Timeout::Error)
+      return 'Time limit exceeded!'
+    end
+    if error.is_a?(NoMemoryError)
+      return 'Memory limit exceeded!'
+    end
+    error.message
   end
 
 end
--- sinatra-controller.rb
+++ sinatra-controller.rb
@@ -1,5 +1,4 @@
 require 'sinatra'
-require 'timeout'
 
 require './game_controller.rb'
 
@@ -10,15 +9,6 @@ end
 post '/play' do
   body = JSON.parse(request.body.read)
   controller = GameController.new(body['strategy1'], body['strategy2'])
-  begin
-    result = Timeout::timeout(2) do
-      result = controller.play_game
-    end
-  rescue Timeout::Error
-    result = Response.new(false, nil, nil, 'Timed out!')
-  rescue NoMemoryError
-    result = Response.new(false, nil, nil, 'Too much memory!')
-  end
-  puts result.game_error
+  result = controller.play_game
   result.to_json
 end
